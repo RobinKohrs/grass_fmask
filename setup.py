1fe21683f612d5f3892ef199845c69c2f6eb54dc
RobinKohrs <robin.kohrs@gmx.de>
2020-02-14 17:27:58 +0100

    ein paar erste python-files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='grass_fmask',
+      version='0.1',
+      description='adding the fmask cloud_filter to grass',
+      url='git@github.com:RobinKohrs/grass_fmask.git',
+      author='Veronica Grupp, Robin Kohrs',
+      author_email='',
+      license='MIT',
+      packages=[''],
+      install_requires=[
+          '',
+      ],
+      zip_safe=False)
