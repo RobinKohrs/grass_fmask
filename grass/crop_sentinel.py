f1075550ef843faf1cff70018420660544eb289d
vroni-g <veronika.grupp@posteo.de>
2020-04-28 19:26:46 +0200

    created script for cropping image data to AOI maintaining the .safe directory structure

--- grass/crop_sentinel.py
+++ grass/crop_sentinel.py
@@ -1,35 +1,57 @@
-# Script to try cropping of sentinel scene to AOI while maintaining .safe directory structure
-
-
-# import rasterio
-# from rasterio.mask import mask
-#
-# band1 = rasterio.open('/Users/veronikagrupp/Documents/UNIVERSIDAD/Jena/wise_1920/grassgis/data/sentinel2_testdata/S2B_MSIL1C_20200327T101629_N0209_R065_T32UPB_20200327T123753.SAFE/GRANULE/L1C_T32UPB_A015964_20200327T101842/IMG_DATA/T32UPB_20200327T101629_B01.jp2',
-#                       driver='JP2OpenJPEG')
-# band1_crop = mask(band1, shapes='/Users/veronikagrupp/Documents/UNIVERSIDAD/Jena/wise_1920/grassgis/data/jena-roda-testsite.geojson',
-#                   all_touched=True,
-#                   crop=True)
-
+# Script to crop sentinel scene to AOI while maintaining .safe directory structure
 
 import fiona
 import rasterio
 import rasterio.mask
+import os
+from glob import glob
+import warnings
 
 
-with fiona.open("/Users/veronikagrupp/Documents/UNIVERSIDAD/Jena/wise_1920/grassgis/data/jena-roda-testsite.shp",
-                "r") as shapefile:
-    shapes = [feature["geometry"] for feature in shapefile]
+def crop_raster(shape, raster):
+    with fiona.open(shape, "r") as shapefile:
+        shapes = [feature["geometry"] for feature in shapefile]
 
-with rasterio.open(
-        "/Users/veronikagrupp/Documents/UNIVERSIDAD/Jena/wise_1920/grassgis/data/sentinel2_testdata/S2B_MSIL1C_20200327T101629_N0209_R065_T32UPB_20200327T123753.SAFE/GRANULE/L1C_T32UPB_A015964_20200327T101842/IMG_DATA/T32UPB_20200327T101629_B01.jp2") as src:
-    out_image, out_transform = rasterio.mask.mask(src, shapes, crop=True)
-    out_meta = src.meta
+    with rasterio.open(raster) as src:
+        out_image, out_transform = rasterio.mask.mask(src, shapes, crop=True)
+        out_meta = src.meta
 
-out_meta.update({"driver": "JP2OpenJPEG",
-                 "height": out_image.shape[1],
-                 "width": out_image.shape[2],
-                 "transform": out_transform})
+    out_meta.update({"driver": "JP2OpenJPEG",
+                     "height": out_image.shape[1],
+                     "width": out_image.shape[2],
+                     "transform": out_transform})
 
-with rasterio.open("/Users/veronikagrupp/Documents/UNIVERSIDAD/Jena/wise_1920/grassgis/data/sentinel2_testdata/S2B_MSIL1C_20200327T101629_N0209_R065_T32UPB_20200327T123753.SAFE/GRANULE/L1C_T32UPB_A015964_20200327T101842/IMG_DATA/T32UPB_20200327T101629_B01.jp2",
-                   "w", **out_meta) as dest:
-    dest.write(out_image)
+    with rasterio.open(raster, "w", **out_meta) as dest:
+        dest.write(out_image)
+
+def iter_crop(dir, shape):
+    glob_pattern = os.path.join(dir, '*')
+    files = glob(glob_pattern)
+    if len(files) != 14:
+        warnings.warn('Usually Sentintel 2 download contains 14 image files. There are ' + str(len(files)) +
+                      ' files in the current directory: ' + dir +
+                      '\n Please check if an error occured in the download etc. \n Cropping proceeds with existing files.')
+
+    for f in files:
+        crop_raster(shape, f)
+
+
+def crop_within_safe(safe_dir, shape):
+    # error if directory does not exist
+
+    gran_folder = os.path.join(safe_dir, 'GRANULE')
+    sub_folder = os.listdir(gran_folder)[0]
+    img_folder = os.path.join(gran_folder, sub_folder, 'IMG_DATA')
+    iter_crop(img_folder, shape)
+
+    # LATER: check if it's one or more safe_dirs, adaption for more than one .safe directory
+
+
+def main():
+    shape = input('Please enter the path to the shapefile of your AOI: '
+    #safe_dir = input('Please enter the path to your .safe directory or a directory containing multiple .safe directories: ')
+    safe_dir = input('Please enter the path to your .safe directory: ')
+    crop_within_safe(safe_dir, shape)
+
+if __name__ == '__main__':
+    main()
