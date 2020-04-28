62aa1278942a361ac7a0986ec00b1412bac75298
vroni-g <veronika.grupp@posteo.de>
2020-04-28 20:05:52 +0200

    Added option fÃor multiple .safe directories

--- grass/crop_sentinel.py
+++ grass/crop_sentinel.py
@@ -9,19 +9,19 @@ import warnings
 
 
 def crop_raster(shape, raster):
-    with fiona.open(shape, "r") as shapefile:
-        shapes = [feature["geometry"] for feature in shapefile]
+    with fiona.open(shape, 'r') as shapefile:
+        shapes = [feature['geometry'] for feature in shapefile]
 
     with rasterio.open(raster) as src:
         out_image, out_transform = rasterio.mask.mask(src, shapes, crop=True)
         out_meta = src.meta
 
-    out_meta.update({"driver": "JP2OpenJPEG",
-                     "height": out_image.shape[1],
-                     "width": out_image.shape[2],
-                     "transform": out_transform})
+    out_meta.update({'driver': 'JP2OpenJPEG',
+                     'height': out_image.shape[1],
+                     'width': out_image.shape[2],
+                     'transform': out_transform})
 
-    with rasterio.open(raster, "w", **out_meta) as dest:
+    with rasterio.open(raster, 'w', **out_meta) as dest:
         dest.write(out_image)
 
 def iter_crop(dir, shape):
@@ -36,22 +36,43 @@ def iter_crop(dir, shape):
         crop_raster(shape, f)
 
 
-def crop_within_safe(safe_dir, shape):
-    # error if directory does not exist
+def get_img_dir(input_dir):
+    # ? add error if directory does not exist
+    if input_dir.endswith('.SAFE'):
+        img_dir = dir_hop(input_dir)
+    else:
+        in_dir = input_dir + os.sep
+        safe_files = [f for f in os.listdir(in_dir) if f.endswith('.SAFE') ]
+        print('There are ' + len(safe_files) + '.SAFE-files in the provided location: \n')
+        print(safe_files)
+        safe_files_full = [os.path.join(input_dir, f) for f in safe_files]
+        img_dir = []
+        for i in safe_files_full:
+            path = dir_hop(i)
+            img_dir.append(path)
 
+    return img_dir
+
+def dir_hop(safe_dir):
     gran_folder = os.path.join(safe_dir, 'GRANULE')
     sub_folder = os.listdir(gran_folder)[0]
     img_folder = os.path.join(gran_folder, sub_folder, 'IMG_DATA')
-    iter_crop(img_folder, shape)
-
-    # LATER: check if it's one or more safe_dirs, adaption for more than one .safe directory
+    return img_folder
 
 
 def main():
-    shape = input('Please enter the path to the shapefile of your AOI: '
-    #safe_dir = input('Please enter the path to your .safe directory or a directory containing multiple .safe directories: ')
-    safe_dir = input('Please enter the path to your .safe directory: ')
-    crop_within_safe(safe_dir, shape)
+    shape = input('Please enter the path to the shapefile of your AOI: ')
+    #shape = '/Users/veronikagrupp/Documents/UNIVERSIDAD/Jena/wise_1920/grassgis/data/jena-roda-testsite.shp'
+    #input_dir = '/Users/veronikagrupp/Documents/UNIVERSIDAD/Jena/wise_1920/grassgis/data/sentinel2_testdata'
+    input_dir = input('Please enter the path to your .safe directory or a directory containing multiple .safe directories: ')
+    safe_dirs = get_img_dir(input_dir)
+
+    if type(safe_dirs)==list:
+        for sd in safe_dirs:
+            iter_crop(sd, shape)
+    else:
+        iter_crop(safe_dirs, shape)
+
 
 if __name__ == '__main__':
     main()
