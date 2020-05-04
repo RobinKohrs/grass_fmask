3e17570d140b23267afb8069725a07a53a5ed959
RobinKohrs <robin.kohrs@gmx.de>
2020-05-04 16:31:44 +0200

    add merge and make timestamp

--- python_workflow/4_merge.py
+++ python_workflow/4_merge.py
@@ -1,10 +1,12 @@
 import os
+import subprocess
 import glob
 from osgeo import gdal
 
-_all = [x for x in os.listdir(".") if x.endswith(".SAFE") and "L1C" in x]
+_all = [x for x in os.listdir(".") if x.endswith(".SAFE") and "L2A" in x]
 
 unique_dates = []
+# get unique dates from .safe-filenames
 for i in _all:
     a =i.split("_")
     b = a[0:3]
@@ -14,6 +16,7 @@ for i in _all:
     else:
         unique_dates.append(date)
 
+# make a dictionary with those dates as keys
 by_date = {date: None for date in unique_dates}
 
 for key in by_date.keys():
@@ -27,8 +30,9 @@ for key in by_date.keys():
     by_date[key] = dates
 
 
-bands = ["B01", "B02", "B03", "B04", "B05", "B06", "B07", "B08", "B09",
-         "B10", "B11", "B12", "B8A"]
+bands = ["B02", "B03", "B04", "B05", "B06", "B07","B11", "B12", "B8A"]
+
+# make a dictionary with the bands as keys and the files to merge as values in a lis
 mergefiles = {band: None for band in bands}
 
 #### MOSAICING #####
@@ -36,20 +40,22 @@ for k,v in by_date.items():
     for key in mergefiles.keys():
         files_to_merge = []
         for safe in v:
-            L1C = glob.glob(os.path.join(safe, "GRANULE", "*", "IMG_DATA"))[0]
-            bands_one_scene = [x for x in os.listdir(L1C) if x.endswith(".jp2")]
+            L2A = glob.glob(os.path.join(safe, "GRANULE", "*", "IMG_DATA", "R20m"))[0]
+            print(L2A)
+            
+            bands_one_scene = [x for x in os.listdir(L2A) if x.endswith(".jp2")]
             for band in bands_one_scene:
                 if key in band:
-                    files_to_merge.append(os.path.join(L1C, band))
+                    files_to_merge.append(os.path.join(L2A, band))
         bands_to_string = " ".join(files_to_merge)
         # all same bands for one date in one string
         #print(bands_to_string)
         #print(" ")
         #print(type(bands_to_string))
         command = "gdal_merge.py  -o {} -of gtiff ".format(bands_to_string.split(os.sep)[-1][0:26]+".tif") + bands_to_string 
-        print(command)
-        print(" ")
-        #subprocess.call(command, shell = True)
+        #print(command)
+        #print(" ")
+        subprocess.call(command, shell = True)
             
 
 
