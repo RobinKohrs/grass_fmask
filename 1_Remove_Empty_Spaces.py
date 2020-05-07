0cd88024a0fdc4116b66dc42329ef920f32a73cd
xFlorian <57533188+xFlorian@users.noreply.github.com>
2020-05-07 14:51:17 +0200

    Delete 1_Remove_Empty_Spaces.py

--- 1_Remove_Empty_Spaces.py
+++
@@ -1,6 +0,0 @@
-#remove empty spaces from a text in python
-with open('D:\Test\sentinel-timestamp.txt', 'r', encoding='utf-8') as inFile,\
-     open('D:\Test\sentinel-timestamp_space.txt', 'w', encoding='utf-8') as outFile:
-    for line in inFile:
-        if line.strip():
-            outFile.write(line)
